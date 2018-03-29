a6649d26ac0747c78e8daf5a80af6c4fca77bdb5
charnesnell <charnes.nell@hotmail.com>
2018-03-29 12:28:51 +1100

    Sandbox
    Unity project to test and play around in

---
+++ Sandbox/Assets/_Game/Scripts/Controller/ButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Game.Scripts.Controller
+{
+    public class ButtonController : MonoBehaviour
+    {
+        public void ResumeGame()
+        {
+            Debug.Log("Resume Game");
+        }
+    }
+}
