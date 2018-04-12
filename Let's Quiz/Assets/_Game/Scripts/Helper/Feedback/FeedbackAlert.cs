a6d92d29f237c2539c8167ae6a7f704592c48d02
charnesnell <charnes.nell@hotmail.com>
2018-04-12 16:04:26 +1000

    Feedback changes
    Added comments to scripts
    Changed the anchor points of the buttons on the two button prefab

--- Let's Quiz/Assets/_Game/Scripts/Helper/Feedback/FeedbackAlert.cs
+++ Let's Quiz/Assets/_Game/Scripts/Helper/Feedback/FeedbackAlert.cs
@@ -11,35 +11,56 @@ namespace _LetsQuiz
         [Header("Component")]
         public GameObject prefab;
 
+        private GameObject _alert;
         private Text _message;
 
         private void Start()
         {
+            // keeps object around so that alert can be called whenever
             DontDestroyOnLoad(gameObject);
         }
 
         public void Show(string message, float time = 2.5f)
         {
-            var alert = (GameObject)Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
-            _message = alert.GetComponentInChildren<Text>();
+            // instantiate the alert
+            _alert = (GameObject)Instantiate(prefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+
+            // find the required components
+            _message = _alert.GetComponentInChildren<Text>();
+
+            // set the message
             _message.text = message;
-            alert.SetActive(true);
-            StartCoroutine(Hide(alert, time));
+
+            // activate the alert
+            _alert.SetActive(true);
+
+            // start coroutine to hide the alert
+            if (time > 0.0f)
+                StartCoroutine(Hide(time));
         }
 
-        private IEnumerator Hide(GameObject alert, float time)
+        private IEnumerator Hide(float time)
         {
+            // wait for x amount of time before hiding alert
             yield return new WaitForSeconds(time);
-            alert.SetActive(false);
-            if (!alert.activeSelf)
-                Destroy(alert);
+
+            // deactivate the alert
+            _alert.SetActive(false);
+
+            // destroy the prefab
+            if (!_alert.activeSelf)
+                Destroy(_alert);
         }
 
-        public void Hide(GameObject alert)
+        // override method incase alert has a display time of 0
+        public void Hide()
         {
-            alert.SetActive(false);
-            if (!alert.activeSelf)
-                Destroy(alert);
+            // deactivate the alert
+            _alert.SetActive(false);
+
+            // destroy the prefab
+            if (!_alert.activeSelf)
+                Destroy(_alert);
         }
     }
 }
--- Let's Quiz/Assets/_Game/Scripts/Helper/Feedback/FeedbackModal.cs
+++ Let's Quiz/Assets/_Game/Scripts/Helper/Feedback/FeedbackModal.cs
@@ -30,34 +30,47 @@ namespace _LetsQuiz
 
         private void Start()
         {
+            // keeps object around so that modal can be called whenever
             DontDestroyOnLoad(gameObject);
 
+            // finds the click helper for audio response
             _click = FindObjectOfType<FeedbackClick>();
         }
 
         private void Initialise(bool oneButton)
         {
+            // determines if one or two button modal needs to be instaniated
             if (oneButton)
             {
+                // instantiate the modal
                 _modal = (GameObject)Instantiate(oneButtonPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+
+                // find the required components
                 _heading = GameObject.FindGameObjectWithTag("Modal_Heading").GetComponent<Text>();
                 _message = GameObject.FindGameObjectWithTag("Modal_Message").GetComponent<Text>();
                 actionButton = GameObject.FindGameObjectWithTag("Modal_Action").GetComponentInChildren<Button>();
                 _action = actionButton.GetComponentInChildren<Text>();
+
+                // add onclick listeners
                 actionButton.onClick.AddListener(Hide);
                 actionButton.onClick.AddListener(_click.Play);
             }
             else
             {
+                // instantiate the modal
                 _modal = (GameObject)Instantiate(twoButtonPrefab, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+
+                // find the required components
                 _heading = GameObject.FindGameObjectWithTag("Modal_Heading").GetComponent<Text>();
                 _message = GameObject.FindGameObjectWithTag("Modal_Message").GetComponent<Text>();
                 negativeButton = GameObject.FindGameObjectWithTag("Modal_Negative").GetComponentInChildren<Button>();
                 _negative = negativeButton.GetComponentInChildren<Text>();
-                negativeButton.onClick.AddListener(Hide);
-                negativeButton.onClick.AddListener(_click.Play);
                 positiveButton = GameObject.FindGameObjectWithTag("Modal_Positive").GetComponentInChildren<Button>();
                 _positive = positiveButton.GetComponentInChildren<Text>();
+
+                // add onclick listeners
+                negativeButton.onClick.AddListener(Hide);
+                negativeButton.onClick.AddListener(_click.Play);
                 positiveButton.onClick.AddListener(Hide);
                 positiveButton.onClick.AddListener(_click.Play);
             }
@@ -65,9 +78,10 @@ namespace _LetsQuiz
 
         public void Show(bool oneButton, string heading, string message, string action = null, string negative = null, string positive = null)
         {
+            // create the modal
             Initialise(oneButton);
 
-            _modal.SetActive(true);
+            // set the heading, message, and button/s text
             _heading.text = heading;
             _message.text = message;
 
@@ -78,11 +92,18 @@ namespace _LetsQuiz
                 _negative.text = negative;
                 _positive.text = positive;
             }
+
+            // activate the modal 
+            _modal.SetActive(true);
+
         }
 
         private void Hide()
         {
+            // deactivate the modal
             _modal.SetActive(false);
+
+            // destory the prefab
             if (!_modal.activeSelf)
                 Destroy(_modal);
         }
