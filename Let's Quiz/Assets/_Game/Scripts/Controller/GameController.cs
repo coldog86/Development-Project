162bc1329d6519938f7ba8b456886746722a7792
charnesnell <charnes.nell@hotmail.com>
2018-04-20 17:11:20 +1000

    Game UI draft
    Added bare bones game UI

---
+++ Let's Quiz/Assets/_Game/Scripts/Controller/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _LetsQuiz
+{
+    public class GameController : MonoBehaviour
+    {
+        // NOTE : PLACEHOLDER
+        public void ReportQuestion()
+        {
+            FeedbackAlert.Show("Report question");
+        }
+
+        // NOTE : PLACEHOLDER
+        public void LikeQuestion()
+        {
+            FeedbackAlert.Show("Like question");
+        }
+    }
+}
+
