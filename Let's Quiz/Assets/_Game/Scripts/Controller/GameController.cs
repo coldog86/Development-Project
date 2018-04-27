bfa9fe29bd1c3e0c003380b60392d8c7775b5c5d
charnesnell <charnes.nell@hotmail.com>
2018-04-27 16:38:04 +1000

    Minor change
    Implemented timer functionality.

--- Let's Quiz/Assets/_Game/Scripts/Controller/GameController.cs
+++ Let's Quiz/Assets/_Game/Scripts/Controller/GameController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Threading;
 
 namespace _LetsQuiz
 {
@@ -8,9 +11,33 @@ namespace _LetsQuiz
     {
         #region variables
 
+        [Header("Components")]
+        public Slider timerBar;
+        public Image timerImage;
+        public Text timerText;
+        public Color timerColorMax;
+        public Color timerColorMid;
+        public Color timerColorMin;
+
+        private float _timeRemaining = 20;
+
         #endregion
 
         #region methods
+
+        #region unity
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+            _timeRemaining -= Time.deltaTime;
+            UpdateTimeRemainingDisplay();
+        }
+
+        #endregion
 
         // NOTE : PLACEHOLDER
         public void ReportQuestion()
@@ -22,6 +49,38 @@ namespace _LetsQuiz
         public void LikeQuestion()
         {
             FeedbackAlert.Show("Like question");
+        }
+
+        #region timer specific
+
+        private void UpdateTimeRemainingDisplay()
+        {
+            var timeRemaining = Mathf.Round(_timeRemaining);
+            timerBar.value = timeRemaining;
+            timerText.text = timeRemaining.ToString();
+
+            if (timerBar.value > 15)
+            {
+                timerImage.color = timerColorMax;
+                timerText.color = timerColorMax;
+            }
+            else if (timerBar.value < 16 && timerBar.value > 5)
+            {
+                timerImage.color = timerColorMid;
+                timerText.color = timerColorMid;
+            }
+            else if (timerBar.value <= 5)
+            {
+                timerImage.color = timerColorMin;
+                timerText.color = timerColorMin;
+            }
+        }
+
+        #endregion
+
+        public void EndGame()
+        {
+            SceneManager.LoadScene(BuildIndex.Result, LoadSceneMode.Single);
         }
 
         #endregion
