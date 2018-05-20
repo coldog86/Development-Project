e6b1c91fef72aeeeafa0436583f88d04288af911
coldog86 <30303169+coldog86@users.noreply.github.com>
2018-05-20 20:44:26 +1000

    Serialisable objects for HighScoreController

---
+++ Let's Quiz/Assets/_Game/Scripts/HighScore/highScoresObject.cs
@@ -0,0 +1,9 @@
+﻿[System.Serializable]
+public class HighScoresObject
+{
+	public string userID;
+	public string userName;
+	public string gamesWon;
+	public string questionsRight;
+	public string totalScore;
+}
