e11bac9945079bc945af1afd2ed995ad5f948411
charnesnell <charnes.nell@hotmail.com>
2018-05-23 17:16:43 +1000

    Minor code change
    Added colour picker to answer button prefab to
    Added Image component reference to remove GetComponent calls

--- Let's Quiz/Assets/_Game/Scripts/Controller/GameController.cs
+++ Let's Quiz/Assets/_Game/Scripts/Controller/GameController.cs
@@ -15,53 +15,55 @@ namespace _LetsQuiz
     public class GameController : MonoBehaviour
     {
         #region variables
-		[Header("Timer")]
+
+        [Header("Timer")]
         public Slider timerBar;
         public Image timerImage;
-        public Color timerColorMax;
-        public Color timerColorMid;
-        public Color timerColorMin;
 
-		private float _timeRemaining = 20;
+        [Header("Color")]
+        public Color timerMin;
+        public Color timerMid;
+        public Color timerMax;
 
-        [Header("header")]
-		public Text scoreDisplay;
+        private float _timeRemaining = 20;
+
+        [Header("Score")]
+        public Text scoreDisplay;
 
         [Header("Question")]
         public Text questionText;
         public GameObject questionDisplay;
-		public QuestionData questionData;
-		public PlayerController player;
-		public QuestionData _currentQuestion;
+        public QuestionData questionData;
+        public PlayerController player;
+        public QuestionData currentQuestion;
 
-		private QuestionData[] questionPool;
-		private int numberOfQuestionsAsked;
+        private QuestionData[] _questionPool;
+        private int _numberOfQuestionsAsked;
 
-		[Header("Answers")]
-		public SimpleObjectPool answerButtonObjectPool;
-		public Transform answerButtonParent;
+        [Header("Answers")]
+        public SimpleObjectPool answerButtonObjectPool;
+        public Transform answerButtonParent;
 
-		[Header ("Controllers")]
-		private DataController _dataController;
-		private RoundData _currentRoundData;
-		private QuestionController _questionController;
-		private PlayerController _playerController;
-		private SubmitScore _submitScore;
+        [Header("Controllers")]
+        private DataController _dataController;
+        private RoundData _currentRoundData;
+        private QuestionController _questionController;
+        private PlayerController _playerController;
+        private SubmitScore _submitScore;
 
-		private bool _isRoundActive;
-		//private int _questionIndex;
-		private FeedbackClick _click;
+        private bool _isRoundActive;
+        //private int _questionIndex;
+        private FeedbackClick _click;
         private FeedbackMusic _music;
 
-        public bool clicked {get; set;}
-		bool isCorrect = false;
-		AnswerButton correctAnswerButton;
-		AnswerData correctAnswerData;
+        public bool clicked { get; set; }
 
-		private List<GameObject> answerButtonGameObjects = new List<GameObject>();
-		AnswerButton userSelection;
+        private bool isCorrect = false;
+        private AnswerButton correctAnswerButton;
+        private AnswerData correctAnswerData;
 
-
+        private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+        private AnswerButton userSelection;
 
         #endregion
 
@@ -71,16 +73,16 @@ namespace _LetsQuiz
 
         private void Start()
         {
-        	_click = FindObjectOfType<FeedbackClick>();
+            _click = FindObjectOfType<FeedbackClick>();
             _music = FindObjectOfType<FeedbackMusic>();
-			_questionController = FindObjectOfType<QuestionController>();
-			_playerController = FindObjectOfType<PlayerController>();
-			_playerController.userScore = 0;
+            _questionController = FindObjectOfType<QuestionController>();
+            _playerController = FindObjectOfType<PlayerController>();
+            _playerController.userScore = 0;
 
-			_questionController.Load ();
-			questionPool = _questionController.getAllQuestionsAllCatagories(); 
+            _questionController.Load();
+            _questionPool = _questionController.getAllQuestionsAllCatagories(); 
 
-			ShowQuestion ();
+            ShowQuestion();
         }
 
         private void Update()
@@ -93,73 +95,82 @@ namespace _LetsQuiz
         }
 
         #endregion
+
         #region display question & answers
 
-		public void ShowQuestion ()
-		{	
-			scoreDisplay.text = _playerController.userScore.ToString();
-			clicked = false;
-			RemoveAnswerButtons ();
+        public void ShowQuestion()
+        {	
+            scoreDisplay.text = _playerController.userScore.ToString();
+            clicked = false;
+            RemoveAnswerButtons();
 
-			QuestionData currentQuestionData = null;
-			if (questionPool.Length <= numberOfQuestionsAsked) { //if all questions are asked, end round
-				Debug.Log ("out of questions");
-				EndRound ();
-			} else {
-				int randomNumber = Random.Range (0, questionPool.Length - 1); //gets random number between 0 and total number of questions
-				currentQuestionData = questionPool [randomNumber];// Get the QuestionData for the current question
-				questionText.text = currentQuestionData.questionText;  // Update questionText with the correct text
-				_questionController.addAskedQuestionToAskedQuestions (currentQuestionData);//keep track of the questions we asked so we can repeat it for the oppoent player
-				numberOfQuestionsAsked++;
-				ShowAnswers(currentQuestionData);
-			}			
-		}
+            QuestionData currentQuestionData = null;
+            if (_questionPool.Length <= _numberOfQuestionsAsked)
+            { //if all questions are asked, end round
+                Debug.Log("out of questions");
+                EndRound();
+            }
+            else
+            {
+                int randomNumber = Random.Range(0, _questionPool.Length - 1); //gets random number between 0 and total number of questions
+                currentQuestionData = _questionPool[randomNumber];// Get the QuestionData for the current question
+                questionText.text = currentQuestionData.questionText;  // Update questionText with the correct text
+                _questionController.addAskedQuestionToAskedQuestions(currentQuestionData);//keep track of the questions we asked so we can repeat it for the oppoent player
+                _numberOfQuestionsAsked++;
+                ShowAnswers(currentQuestionData);
+            }			
+        }
 
-		private void ShowAnswers(QuestionData currentQuestionData)
-		{
+        private void ShowAnswers(QuestionData currentQuestionData)
+        {
 			
-			List<int> answerText = new List<int> ();
-			Random rnd = new Random ();
-			for (int i = 0; i < currentQuestionData.answers.Length; i++) {  // For every AnswerData in the current QuestionData...
+            List<int> answerText = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < currentQuestionData.answers.Length; i++)
+            {  // For every AnswerData in the current QuestionData...
 			
-				int n = Random.Range (0, currentQuestionData.answers.Length);
-				while (answerText.Contains (n)) {
-					n = Random.Range (0, currentQuestionData.answers.Length);//randomise where the answers are displayed
-				}
-				answerText.Add (n);
-				GameObject answerButtonGameObject = answerButtonObjectPool.GetObject ();// Spawn an AnswerButton from the object pool
-				answerButtonGameObjects.Add (answerButtonGameObject);
+                int n = Random.Range(0, currentQuestionData.answers.Length);
+                while (answerText.Contains(n))
+                {
+                    n = Random.Range(0, currentQuestionData.answers.Length);//randomise where the answers are displayed
+                }
+                answerText.Add(n);
+                GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();// Spawn an AnswerButton from the object pool
+                answerButtonGameObjects.Add(answerButtonGameObject);
 
-				answerButtonGameObject.transform.SetParent (answerButtonParent);
-				answerButtonGameObject.transform.localScale = Vector3.one; //I was having an issue were the scale blew out, this fixed it...
-				AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton> ();	
-				answerButton.SetUp (currentQuestionData.answers [n]);// Pass the AnswerData to the AnswerButton to check if it correct
+                answerButtonGameObject.transform.SetParent(answerButtonParent);
+                answerButtonGameObject.transform.localScale = Vector3.one; //I was having an issue were the scale blew out, this fixed it...
+                AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();	
+                answerButton.SetUp(currentQuestionData.answers[n]);// Pass the AnswerData to the AnswerButton to check if it correct
 
-				if (answerButton.isCorrect (currentQuestionData.answers [n])) {
-					correctAnswerButton = answerButton;
-					isCorrect = true;
-					correctAnswerData = currentQuestionData.answers [n];
-				}
-			}
-		}
+                if (answerButton.isCorrect(currentQuestionData.answers[n]))
+                {
+                    correctAnswerButton = answerButton;
+                    isCorrect = true;
+                    correctAnswerData = currentQuestionData.answers[n];
+                }
+            }
+        }
 
-		#endregion
+        #endregion
 
-		public AnswerButton getCorrectAnswerButton() //getter used by the answerButton, I got an error when i tryed to declar with the variable at the top
-		{
-			return correctAnswerButton;
-		}
+        public AnswerButton getCorrectAnswerButton() //getter used by the answerButton, I got an error when i tryed to declar with the variable at the top
+        {
+            return correctAnswerButton;
+        }
 
 
-		void RemoveAnswerButtons ()  // Return all spawned AnswerButtons to the object pool
-		{
-			while (answerButtonGameObjects.Count > 0) {
-				answerButtonObjectPool.ReturnObject (answerButtonGameObjects [0]);
-				answerButtonGameObjects.RemoveAt (0);
-			}
-		}
+        void RemoveAnswerButtons()  // Return all spawned AnswerButtons to the object pool
+        {
+            while (answerButtonGameObjects.Count > 0)
+            {
+                answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+                answerButtonGameObjects.RemoveAt(0);
+            }
+        }
 
-		#region like & dislike buttons
+        #region like & dislike buttons
+
         // NOTE : placeholder
         public void ReportQuestion()
         {
@@ -168,30 +179,31 @@ namespace _LetsQuiz
         }
 
         // NOTE : placeholder
-        public void LikeQuestion ()
-		{
-			_click.Play ();
-			FeedbackAlert.Show ("Like question");
-		}
-		#endregion
+        public void LikeQuestion()
+        {
+            _click.Play();
+            FeedbackAlert.Show("Like question");
+        }
 
-		#region user stuff
+        #endregion
 
-		public void Score(bool answer)
-    	{
-    		Debug.Log("score called bool = " + answer);
-			if (answer)
-				_playerController.userScore = _playerController.userScore + 10;
+        #region user stuff
 
-			if (!answer)
-				_playerController.userScore = _playerController.userScore - 5;
+        public void Score(bool answer)
+        {
+            Debug.Log("score called bool = " + answer);
+            if (answer)
+                _playerController.userScore = _playerController.userScore + 10;
 
-			ShowQuestion();
-    	}
+            if (!answer)
+                _playerController.userScore = _playerController.userScore - 5;
 
-		#endregion
+            ShowQuestion();
+        }
 
-         #region timer specific
+        #endregion
+
+        #region timer specific
 
         private void UpdateTimeRemainingDisplay()
         {
@@ -199,29 +211,35 @@ namespace _LetsQuiz
             timerBar.value = timeRemaining;
 
             if (timerBar.value > 15)
-                timerImage.color = timerColorMax;
+                timerImage.color = timerMax;
             else if (timerBar.value < 16 && timerBar.value > 6)
-                timerImage.color = timerColorMid;
+                timerImage.color = timerMid;
             else if (timerBar.value <= 5)
-                timerImage.color = timerColorMin;
+                timerImage.color = timerMin;
         }
 
         #endregion
 
-        public void EndRound ()
-		{
-			_music.Stop ();
-			if (_playerController.userScore > _playerController.GetHighestScore ()) 
-			{
-				Debug.Log("new high score");
-				_playerController.SetHighestScore(_playerController.userScore);
-				_submitScore = FindObjectOfType<SubmitScore> ();
-				_submitScore.SubmitScores (_playerController.GetUsername(), _playerController.GetHighestScore ());
-			}
+        public void EndRound()
+        {
+            _music.Stop();
+
+            if (_playerController.userScore > _playerController.GetHighestScore())
+            {
+                Debug.Log("new high score");
+                _playerController.scoreStatus = "new high score";
+                _playerController.SetHighestScore(_playerController.userScore);
+                _submitScore = FindObjectOfType<SubmitScore>();
+                _submitScore.SubmitScores(_playerController.GetUsername(), _playerController.GetHighestScore());
+            }
+            else
+            {
+                _playerController.scoreStatus = "no change";
+            }
+
             SceneManager.LoadScene(BuildIndex.Result, LoadSceneMode.Single);
             Debug.Log("end round");
-
-
+            Debug.Log(_playerController.scoreStatus);
         }
 
         #endregion
--- Let's Quiz/Assets/_Game/Scripts/Object/AnswerButton.cs
+++ Let's Quiz/Assets/_Game/Scripts/Object/AnswerButton.cs
@@ -5,80 +5,82 @@ using UnityEngine.SceneManagement;
 using System.IO;
 using System.Collections.Generic;
 
-namespace _LetsQuiz{
+namespace _LetsQuiz
+{
+    public class AnswerButton : MonoBehaviour
+    {
+        [Header("Component")]
+        public Text answerText;
+        public Button answerButton;
+        public Image buttonImage;
 
-	public class AnswerButton : MonoBehaviour 
-	{
-		public Text answerText;
-		public Button answerButton;
+        [Header("Colour")]
+        public Color original;
+        public Color red;
+        public Color green;
 
-		private AnswerData answerData;
-		private GameController _GameController; 
+        private AnswerData answerData;
+        private GameController _gameController;
 
-		void Start()
-		{
-			_GameController = FindObjectOfType<GameController>();
-		}
+        void Start()
+        {
+            _gameController = FindObjectOfType<GameController>();
+            buttonImage = GetComponent<Image>();
+        }
 
-		public void SetUp(AnswerData data)
-		{
-			answerData = data;
-			answerText.text = answerData.answerText;
-		}
+        public void SetUp(AnswerData data)
+        {
+            answerData = data;
+            answerText.text = answerData.answerText;
+        }
 
-		public bool isCorrect(AnswerData data){
-			bool isCorrect = data.isCorrect;
-			return isCorrect;
-		}
+        public bool isCorrect(AnswerData data)
+        {
+            bool isCorrect = data.isCorrect;
+            return isCorrect;
+        }
 
-		public void HandleAnswerClick()
-		{
-			if(!_GameController.clicked) //prevents players from selecting multiple answers
-			{
-				_GameController.clicked = true;
-				if (!answerData.isCorrect) 	
-					changeToRed ();
-			 	else 
-					changeToGreen ();
-			}
+        public void HandleAnswerClick()
+        {
+            if (!_gameController.clicked) //prevents players from selecting multiple answers
+            {
+                _gameController.clicked = true;
 
-			
-		
-		}
-
-
-
-
-		public void changeToGreen()
-		{
-			StartCoroutine("green");
-		}
-		IEnumerator green()
-		{
-			Color prevColor = GetComponent<Image> ().color; //get color that buttons are so we can change them back.
-			GetComponent<Image> ().color = Color.green; //set wrong push to red
-			yield return new WaitForSeconds(1); //give people a chance to see 
-			GetComponent<Image> ().color = prevColor; //change color back
-			_GameController.Score(answerData.isCorrect);
-		}
-
-		public void changeToRed()
-		{
-			StartCoroutine("red");
-		}
-		IEnumerator red()
-		{
-			Color prevColor = GetComponent<Image> ().color; //get color that buttons are so we can change them back.
-			GetComponent<Image> ().color = Color.red; //set wrong push to red
-			AnswerButton rightButton = _GameController.getCorrectAnswerButton();  //get the correct answer
-			rightButton.GetComponent<Image> ().color = Color.green; //change it to green
-			yield return new WaitForSeconds(1); //give people a chance to see 
-			GetComponent<Image> ().color = prevColor; //change wrong answer color back 
-			rightButton.GetComponent<Image> ().color = prevColor; //change correct answer color back
-			_GameController.Score (answerData.isCorrect);
-
-		}
+                if (!answerData.isCorrect)
+                    changeToRed();
+                else
+                    changeToGreen();
+            }		
+        }
 
 
-	}
+        public void changeToGreen()
+        {
+            StartCoroutine("Green");
+        }
+
+        IEnumerator Green()
+        {
+            buttonImage.color = green; //set wrong push to red
+            yield return new WaitForSeconds(1); //give people a chance to see 
+            buttonImage.color = original; //change color back
+            _gameController.Score(answerData.isCorrect);
+        }
+
+        public void changeToRed()
+        {
+            StartCoroutine("Red");
+        }
+
+        IEnumerator Red()
+        {
+            buttonImage.color = red; //set wrong push to red
+            AnswerButton rightButton = _gameController.getCorrectAnswerButton();  //get the correct answer
+            rightButton.buttonImage.color = green; //change it to green
+            yield return new WaitForSeconds(1); //give people a chance to see 
+            buttonImage.color = original; //change wrong answer color back 
+            rightButton.buttonImage.color = original; //change correct answer color back
+            _gameController.Score(answerData.isCorrect);
+        }
+    }
 }
