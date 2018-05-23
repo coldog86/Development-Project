c301d754f5f849c63f4c386d461a29fdd694d4f1
charnesnell <charnes.nell@hotmail.com>
2018-05-23 17:18:28 +1000

    Android back button helper
    Backward navigation for android devices

---
+++ Let's Quiz/Assets/_Game/Scripts/Helper/AndroidBackHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _LetsQuiz
+{
+    #if PLATFORM_ANDROID
+    public class AndroidBackHelper : MonoBehaviour
+    {
+        public void Update()
+        {
+            if (SceneManager.GetActiveScene().buildIndex == BuildIndex.Splash || SceneManager.GetActiveScene().buildIndex == BuildIndex.Login || SceneManager.GetActiveScene().buildIndex == BuildIndex.Menu)
+            {
+                if (Input.GetKey(KeyCode.Escape))
+                    FeedbackTwoButtonModal.Show("Are you sure?", "Are you sure you want to quit?", "Yes", "No", Application.Quit, FeedbackTwoButtonModal.Hide);
+                
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.Escape))
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1, LoadSceneMode.Single);
+            }
+                
+        }
+    }
+    #endif
+}
+
