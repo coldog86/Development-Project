c62e8892e0f97e83358118f7312853c2c36049eb
charnesnell <charnes.nell@hotmail.com>
2018-05-25 16:55:19 +1000

    Expanded Android Helper
    Expanded android helper to forfeit game when quitting.
    Fixed if statement to go back to menu when needed.

--- Let's Quiz/Assets/_Game/Scripts/Helper/AndroidBackHelper.cs
+++ Let's Quiz/Assets/_Game/Scripts/Helper/AndroidBackHelper.cs
@@ -7,23 +7,42 @@ namespace _LetsQuiz
 {
     public class AndroidBackHelper : MonoBehaviour
     {
+        private GameController _gameController;
+
         #if PLATFORM_ANDROID
         public void Update()
         {
-            if (SceneManager.GetActiveScene().buildIndex == BuildIndex.Splash || SceneManager.GetActiveScene().buildIndex == BuildIndex.Login || SceneManager.GetActiveScene().buildIndex == BuildIndex.Menu)
+            if (SceneManager.GetActiveScene().buildIndex == BuildIndex.Splash ||
+                SceneManager.GetActiveScene().buildIndex == BuildIndex.Login ||
+                SceneManager.GetActiveScene().buildIndex == BuildIndex.Menu)
             {
                 if (Input.GetKey(KeyCode.Escape))
                     FeedbackTwoButtonModal.Show("Are you sure?", "Are you sure you want to quit?", "Yes", "No", Application.Quit, FeedbackTwoButtonModal.Hide);
                 
             }
-            else
+            else if (SceneManager.GetActiveScene().buildIndex == BuildIndex.Account ||
+                     SceneManager.GetActiveScene().buildIndex == BuildIndex.Leaderboard ||
+                     SceneManager.GetActiveScene().buildIndex == BuildIndex.SubmitQuestion ||
+                     SceneManager.GetActiveScene().buildIndex == BuildIndex.Settings)
             {
                 if (Input.GetKey(KeyCode.Escape))
-                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1, LoadSceneMode.Single);
+                    SceneManager.LoadScene(BuildIndex.Menu, LoadSceneMode.Single);
             }
-                
+            else if (SceneManager.GetActiveScene().buildIndex == BuildIndex.Game)
+            {
+                if (Input.GetKey(KeyCode.Escape))
+                {
+                    _gameController = FindObjectOfType<GameController>();
+                    FeedbackTwoButtonModal.Show("Are you sure?", "Are you sure you want to forfeit this game?", "Yes", "No", ForfeitGame, FeedbackTwoButtonModal.Hide);
+                }    
+            }
         }
         #endif
+
+        private void ForfeitGame()
+        {
+            _gameController.EndRound();
+        }
     }
 }
 
