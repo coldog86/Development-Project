755bba63e87634b66af0e86a350e52aa87aa46b7
charnesnell <charnes.nell@hotmail.com>
2018-05-25 14:21:09 +1000

    Bug fix
    Found a bug where application didn't display error modal on download error

--- Let's Quiz/Assets/_Game/Scripts/Database/GetHighScores.cs
+++ Let's Quiz/Assets/_Game/Scripts/Database/GetHighScores.cs
@@ -4,75 +4,76 @@ using UnityEngine;
 
 namespace _LetsQuiz
 {
-	public class GetHighScores : MonoBehaviour
-	{
-		#region variables
+    public class GetHighScores : MonoBehaviour
+    {
+        #region variables
 
-		private float _downloadTimer = 5.0f;
+        private float _downloadTimer = 5.0f;
 
-		private DataController _dataController;
-		private PlayerController _playerController;
+        private DataController _dataController;
+        private PlayerController _playerController;
 
-		#endregion
+        #endregion
 
-		#region methods
+        #region methods
 
-		#region unity
+        #region unity
 
-		private void Start()
-		{
-			_dataController = FindObjectOfType<DataController>();
-			_playerController = FindObjectOfType<PlayerController>();
-		}
+        private void Start()
+        {
+            _dataController = FindObjectOfType<DataController>();
+            _playerController = FindObjectOfType<PlayerController>();
+        }
 
-		#endregion
+        #endregion
 
-		#region download specific
+        #region download specific
 
-		public IEnumerator PullAllHighScoresFromServer()
-		{
-			WWW download = new WWW(ServerHelper.Host + ServerHelper.HighScores);
-			while (!download.isDone)
-			{
-				if (_downloadTimer < 0)
-				{
-					Debug.LogError("Server time out.");
-					_dataController.serverConnected = false;
-					break;
-				}
-				_downloadTimer -= Time.deltaTime;
+        public IEnumerator PullAllHighScoresFromServer()
+        {
+            WWW download = new WWW(ServerHelper.Host + ServerHelper.HighScores);
+            while (!download.isDone)
+            {
+                if (_downloadTimer < 0)
+                {
+                    Debug.LogError("Server time out.");
+                    _dataController.serverConnected = false;
+                    break;
+                }
+                _downloadTimer -= Time.deltaTime;
 
-				yield return null;
-			}
+                yield return null;
+            }
 
-			if (!download.isDone || download.error != null)
-			{
-				/* if we cannot connect to the server or there is some error in the data, 
+            if (!download.isDone || download.error != null)
+            {
+                /* if we cannot connect to the server or there is some error in the data, 
                  * check the prefs for previously saved questions */
-				Debug.LogError(download.error);
-				Debug.Log("Failed to hit the server.");
-				_dataController.serverConnected = false;               
-			}
-			else
-			{ 
-				// we got the string from the server, it is every question in JSON format
-				Debug.Log("Vox transmition recieved");
-				Debug.Log(download.text);
+                Debug.LogError(download.error);
+                Debug.Log("Failed to hit the server.");
+                _dataController.serverConnected = false;               
+            }
+            else
+            { 
+                // we got the string from the server, it is every question in JSON format
+                Debug.Log("Vox transmition recieved");
+                Debug.Log(download.text);
 
-				_dataController.serverConnected = true;
-				_dataController.allHighScoreJSON = download.text;
-				//_leaderboardController.setHighScoreData(download.text);
+                _dataController.serverConnected = true;
+                _dataController.allHighScoreJSON = download.text;
+                //_leaderboardController.setHighScoreData(download.text);
 
-				yield return download;
+                yield return download;
 
-				_playerController.SetHighscoreData (download.text);
-			} 
-		}
+                _playerController.SetHighscoreData(download.text);
+                _dataController.Init();
+            } 
+        }
 
-		#endregion
+        #endregion
 
-		#endregion
+        #endregion
 
-	}
+    }
 }
 
