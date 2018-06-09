e9af84f174b957c6fc1db73c9a4d95ff2cf1ed27
coldog86 <30303169+coldog86@users.noreply.github.com>
2018-06-09 12:36:36 +1000

    minor change and additon

--- Let's Quiz/Assets/_Game/Scripts/Helper/ServerHelper.cs
+++ Let's Quiz/Assets/_Game/Scripts/Helper/ServerHelper.cs
@@ -6,7 +6,8 @@
         public static string DownloadQuestion = "pullAllQuestions.php";
         public static string Login = "loginExistingUserWithEmail.php";
         public static string Register = "addUser.php";
-        public static string Ranking = "ranking.php";
+        public static string GetRanking = "getRanking.php";
+        public static string SetRanking = "setRanking.php";
         public static string HighScores = "viewHighScores.php";
         public static string SubmitHighScore = "submitHighScore.php";
         public static string SubmitUserQuestion = "submitQuestion.php";
