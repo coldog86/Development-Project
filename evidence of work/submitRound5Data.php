c3b265b6f58f9f9ad4d8d1fe32f8288448466faa
coldog86 <30303169+coldog86@users.noreply.github.com>
2018-07-23 18:25:12 +1000

    Create submitRound5Data.php

---
+++ evidence of work/submitRound5Data.php
@@ -0,0 +1,59 @@
+
+<?php
+
+//Variables for the connection
+	$servername = "localhost";
+	$server_username =  "u697839110_col";
+	$server_password = "collin";
+	$dbName = "u697839110_40k";
+	
+	 $gameNumber = $_POST["gameNumberPost"];
+	 $score = $_POST["scorePost"];
+	 $turnNumber = $_POST["turnsCompletedPost"];
+	 
+	//Make Connection
+	$conn = new mysqli($servername, $server_username, $server_password, $dbName);
+	//Check Connection
+	if(!$conn){
+		die("Connection Failed. ". mysqli_connect_error());
+	}
+	
+    $sql = "UPDATE OngoingGames 
+            SET Score=0, turnNumber=?
+            WHERE gameNumber=$gameNumber
+            ";
+                            
+    if($stmt = mysqli_prepare($conn, $sql)){
+    
+        mysqli_stmt_bind_param($stmt, "i", $turnNumber);
+    
+        
+        $stmt->execute();
+        
+        echo "Ongoing game updated successfully.";
+        
+    } 
+
+	  $sql = "
+            UPDATE Users 
+        SET numGamesPlayed=$totalGamesPlayed, totalPointsScore=totalPointsScore + $score, TotalCorrectAnswers=$totalQuestions, TotalCorrectAnswers=$totalCorrectQuestions
+        WHERE ID=$userID;
+        ";                 
+                            
+    if($stmt = mysqli_prepare($conn, $sql)){
+    
+        mysqli_stmt_bind_param($stmt, "issssii", $gameNumber, $player, $askedQuestions, $QuestionsLeftInCatagory, $Round1Catagory, $score, $turnNumber);
+    
+        
+        $stmt->execute();
+        
+        echo "Ongoing game updated successfully.";
+        
+    } 
+	
+	
+
+	
+
+
+?>
