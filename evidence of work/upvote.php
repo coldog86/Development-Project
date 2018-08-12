56e75a97ca46f174dffdf7286d89b49b692ae16a
coldog86 <30303169+coldog86@users.noreply.github.com>
2018-08-13 08:19:29 +1000

    Create upvote.php

---
+++ evidence of work/upvote.php
@@ -0,0 +1,38 @@
+
+<?php
+
+//Variables for the connection
+	$servername = "localhost";
+	$server_username =  "u697839110_col";
+	$server_password = "collin";
+	$dbName = "u697839110_40k";
+	
+	 $questionText = $_POST["questionPost"];
+	 
+	 
+	//Make Connection
+	$conn = new mysqli($servername, $server_username, $server_password, $dbName);
+	//Check Connection
+	if(!$conn){
+		die("Connection Failed. ". mysqli_connect_error());
+	}
+	
+    $sql = "UPDATE q2 
+set Upvotes = Upvotes+1 
+WHERE QuestionText = ?";
+                            
+    if($stmt = mysqli_prepare($conn, $sql)){
+    
+        mysqli_stmt_bind_param($stmt, "s", $questionText);
+    
+        
+        $stmt->execute();
+        
+        echo "Records inserted successfully.";
+        
+    } 
+
+	
+
+
+?>
